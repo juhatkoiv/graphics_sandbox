6d5e6b68f7ef655a069e2bf9191fed5efea84489
Juha <juha.t.koivistoinen@gmail.com>
2024-10-16 17:42:47 +0300

    add additional debug

--- Graphics_Sandbox/Rendering/OpenGL/GfxDeviceOpenGL.cpp
+++ Graphics_Sandbox/Rendering/OpenGL/GfxDeviceOpenGL.cpp
@@ -8,8 +8,27 @@
 
 BEGIN_NAMESPACE1( rendering )
 
+void APIENTRY messageCallback( GLenum source,
+							   GLenum type,
+							   GLuint id,
+							   GLenum severity,
+							   GLsizei length,
+							   const GLchar* message,
+							   const void* userParam )
+{
+	if (type != GL_DEBUG_TYPE_ERROR)
+		return;
+
+	std::cerr << "GL CALLBACK: " << (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : "")
+		<< " type = " << type << ", severity = " << severity
+		<< ", message = " << message << std::endl;
+}
+
 static void setErrorCallback()
 {
+	glEnable( GL_DEBUG_OUTPUT );
+	glDebugMessageCallback( messageCallback, 0 );
+
 	glfwSetErrorCallback( []( int error_code, const char* description )
 		{
 			Logger::LogError( description );
